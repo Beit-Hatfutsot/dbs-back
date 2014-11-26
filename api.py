10ad59d6c0e52762c714ff1fb91710954fffd5ea
Pavel Suchman <pavel.suchman@gmail.com>
2014-11-26 17:38:47 +0200

    Implemented user management
    On Trello - https://trello.com/c/bLxQpJ76
    
    ToDo:
    - agree on standard response firmat for all user operations
    - implement masking where sensitive user information is revealed
    - Implement Json responses for abort style esceptions
    - Write tests - FULL COVERAGE - IMPORTANT!!!

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 from datetime import timedelta
+import json
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from flask.ext.mongoengine import MongoEngine
 from flask.ext.security import Security, MongoEngineUserDatastore, \
     UserMixin, RoleMixin, login_required
@@ -74,14 +75,22 @@ class User(db.Document, UserMixin):
     password = db.StringField(max_length=255)
     active = db.BooleanField(default=True)
     confirmed_at = db.DateTimeField()
-    roles = db.ListField(db.ReferenceField(Role), default=['user'])
+    roles = db.ListField(db.ReferenceField(Role))
 
 # Ensure we have a user to test with
 @app.before_first_request
-def create_user():
-    if not user_datastore.get_user('dannybmail@gmail.com'):
+def setup_users():
+    for role_name in ('user', 'admin'):
+        if not user_datastore.find_role(role_name):
+            logger.debug('Creating role %s' % role_name)
+            user_datastore.create_role(name=role_name)
+
+    user_role = user_datastore.find_role('user')
+    if not user_datastore.get_user('tester@example.com'):
         logger.debug('Creating test user.')
-        user_datastore.create_user(email='dannybmail@gmail.com', password=encrypt_password('password'))
+        user_datastore.create_user(email='tester@example.com',
+                                   password=encrypt_password('password'),
+                                   roles=[user_role])
 
 # Setup Flask-Security
 user_datastore = MongoEngineUserDatastore(db, User, Role)
@@ -92,6 +101,84 @@ def humanify(obj):
     resp = jsonify(obj)
     resp.set_data(resp.data+'\n')
     return resp
+
+def is_admin(flask_user_obj):
+    if flask_user_obj.has_role('admin'):
+        return True
+    else:
+        return False
+
+def user_handler(user_id, method, data):
+    if data:
+        try:
+            data = json.loads(data)
+        except ValueError:
+            logger.debug('Could not decode JSON from data')
+            abort(400)
+
+    if method == 'GET':
+        return humanify(get_user(user_id))
+
+    elif method == 'POST':
+        return humanify(create_user(data))
+
+    elif method == 'PUT':
+        return humanify(update_user(user_id, data))
+
+    elif method == 'DELETE':
+        return humanify(delete_user(user_id))
+
+def _get_user_or_error(user_id):
+    user = user_datastore.get_user(user_id)
+    if user:
+        return user
+    else:
+        raise abort(404)
+
+def get_user(user_id):
+    user = _get_user_or_error(user_id)
+    return json.loads(user.to_json())
+
+def delete_user(user_id):
+    user = _get_user_or_error(user_id)
+    if is_admin(user):
+        return {'error': 'God Mode!'}
+    else:
+        user.delete()
+        return {'deleted': user_id}
+
+def create_user(user_dict):
+    try:
+        email = user_dict['email']
+        enc_password = encrypt_password(user_dict['password'])
+    except KeyError as e:
+        logger.debug('%s key is missing from data' % e)
+        abort(400)
+
+    user_exists = user_datastore.get_user(email)
+    if user_exists:
+        logger.debug('User %s with email %s already exists' % (str(user_exists.id), email))
+        abort(409)
+        #return {'error': 'Email exists'}
+
+    created = user_datastore.create_user(email=email,
+                                        password=enc_password)
+    # Add default role to a newly created user
+    user_datastore.add_role_to_user(created, 'user')
+
+    return {'created': str(created.id)}
+
+def update_user(user_id, user_dict):
+    user = _get_user_or_error(user_id)
+    if 'email' in user_dict.keys():
+        user.email = user_dict['email']
+    if 'password' in user_dict.keys():
+        enc_password = encrypt_password(user_dict['password'])
+        user.password = enc_password
+
+    return {'updated': user,
+            'id': user_id}
+
 
 # Views
 @app.route('/')
@@ -112,13 +199,36 @@ def private_space():
 
 @app.route('/user', methods=['GET', 'POST', 'PUT', 'DELETE'])
 @app.route('/user/<user_id>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-@jwt_required()
 def manage_user(user_id=None):
-    if request.method == 'GET':
-        user_obj = current_user
-        user_obj.id = str(user_obj.id)
-        return humanify({'id': user_obj.id,
-                         'email': user_obj.email})
+    '''
+    Manage user accounts. If routed as /user, gives access only to logged in
+    user, if routed as /user/<user_id>, allows administrative level access
+    if the looged in user is in the admin group.
+    POST gets special treatment, as there must be a way to register new user.
+    '''
+    try:
+        verify_jwt()
+    except JWTError as e:
+        # You can create a new user while not being logged in
+        # Will have to defend this endpoint with rate limiting or similar
+        if request.method == 'POST':
+            return user_handler(None, request.method, request.data)
+        else:
+            logger.debug(e.description)
+            abort(403)
+
+    if user_id:
+        # access_mode = 'admin'
+        if is_admin(current_user):
+            return user_handler(user_id, request.method, request.data)
+        else:
+            logger.debug('Non-admin user %s tried to access user id %s' % (current_user.email, user_id))
+            abort(403)
+    else:
+        # access_mode = 'user'
+        user_id = str(current_user.id)
+        return user_handler(user_id, request.method, request.data)
+
 
 if __name__ == '__main__':
     logger.debug('Starting api')
