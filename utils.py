8c57d6a3b3a43958aed0e9fe8abbfcaf602c03ca
Pavel Suchman <pavel.suchman@gmail.com>
2015-01-08 14:51:28 +0200

    Implemented jsonify to take care of bson object ids

--- utils.py
+++ utils.py
@@ -1,8 +1,28 @@
 import logging
+import json
+import datetime
 
 import yaml
 import bson
+from bson.objectid import ObjectId
+from werkzeug import Response
 
+class MongoJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime, datetime.date)):
+            return obj.isoformat()
+        elif isinstance(obj, ObjectId):
+            return unicode(obj)
+        return json.JSONEncoder.default(self, obj)
+
+def jsonify(*args, **kwargs):
+    """ jsonify with support for MongoDB ObjectId
+        See https://gist.github.com/akhenakh/2954605
+    """
+    return Response(json.dumps(dict(*args, **kwargs),
+                    indent=2,
+                    cls=MongoJsonEncoder),
+                    mimetype='application/json')
 
 class Struct:
     def __init__(self, **entries): 
