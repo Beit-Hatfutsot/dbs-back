c78ce48550d93e3eeeff5b001ad678f9f8321aa2
Pavel Suchman <pavel.suchman@gmail.com>
2015-01-21 23:21:56 +0200

    A simple SoundCloud uploader - initial version

---
+++ soundcloud_uploader.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import glob
+import os
+
+import pymongo
+
+import utils
+
+config_file = '/home/bhs/.soundcloud'
+
+sc_client = utils.get_sc_client(config_file)
+
+db = pymongo.Connection()['bh']
+collection = db['uploaded_music']
+
+music_dir = '/home/bhs/bhp_storage/Music'
+
+os.chdir(music_dir)
+for fn in glob.glob("*.mp3"):
+    try:
+        track = utils.upload_to_soundcloud(sc_client, fn)
+        print 'Uploaded {} to soundcloud - got id {} at {}'.format(fn, track['id'], track['permalink_url'])
+        collection.insert(track)
+    except Exception as e:
+        print 'Error uploading {} to Soundcloud: {}'.format(fn, e.message)
+        collection.insert({'filename': fn, 'Exception': e})
