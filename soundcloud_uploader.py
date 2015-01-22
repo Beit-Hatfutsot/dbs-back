9e96de8e655be5467dea13b310b724a60ad83ff3
Pavel Suchman <pavel.suchman@gmail.com>
2015-01-22 14:09:34 +0200

    Improved the uploader script
    - Bug fix: Don't try to save exception object in a Mongo document
    - Feature: Insert a unique identifier (filename) to database and don't upload
    the same item more then once

--- soundcloud_uploader.py
+++ soundcloud_uploader.py
@@ -18,10 +18,15 @@ music_dir = '/home/bhs/bhp_storage/Music'
 
 os.chdir(music_dir)
 for fn in glob.glob("*.mp3"):
+    saved = collection.find_one({'filename': fn})
+    if saved and not saved.has_key('Exception'):
+        print 'File {} was already uploaded - skipping it'.format(fn)
+        continue
     try:
         track = utils.upload_to_soundcloud(sc_client, fn)
+        track['filename'] = fn
         print 'Uploaded {} to soundcloud - got id {} at {}'.format(fn, track['id'], track['permalink_url'])
         collection.insert(track)
     except Exception as e:
         print 'Error uploading {} to Soundcloud: {}'.format(fn, e.message)
-        collection.insert({'filename': fn, 'Exception': e})
+        collection.insert({'filename': fn, 'Exception': e.message})
