fc8a3e80c55f1cbb9fe724cc1432d2b5989ab8b4
Pavel Suchman <pavel.suchman@gmail.com>
2015-09-21 18:43:14 +0300

    Move the elasticsearch functionality to api
    - Delete Redis

--- batch_related.py
+++ batch_related.py
@@ -3,23 +3,8 @@
 import datetime
 import logging
 
-import redis
-from elasticsearch import Elasticsearch
-
 import api
 
-def save_redis_list(redis, key, values):
-    """A naive wrapper around rpush"""
-    for v in values:
-        redis.rpush(key, v)
-
-def get_redis_list(redis, key):
-    """A naive wrapper around lrange"""
-    try:
-        return redis.lrange(key, 0, -1)
-    except AttributeError:
-        # Key not found
-        return None
 
 def get_now_str():
     format = '%d.%h-%H:%M:%S'
@@ -27,25 +12,6 @@ def get_now_str():
     now_str = datetime.datetime.strftime(now, format)
     return now_str
 
-def es_mlt_search(index_name, doc_type, doc_id, doc_fields, target_doc_type, limit):
-    '''Build an mlt query and execute it'''
-    query = {'query':
-                {'mlt':
-                    {'docs': [
-                        {'_id': doc_id,
-                        '_index': index_name,
-                        '_type': doc_type}],
-                    'fields': doc_fields
-                    }
-                }
-            }
-    es = Elasticsearch('localhost')
-    results = es.search(doc_type=target_doc_type, body=query, size=limit)
-    if len(results['hits']['hits']) > 0:
-        result_doc_ids = ['{}.{}'.format(h['_type'], h['_source']['_id']) for h in results['hits']['hits']]
-        return result_doc_ids
-    else:
-        return None
 
 if __name__ == '__main__':
     collections = api.SEARCHABLE_COLLECTIONS
@@ -53,9 +19,8 @@ if __name__ == '__main__':
     db = api.data_db
     index_name = 'bhp10'
     related_fields = ['Header.En', 'UnitText1.En', 'Header.He', 'UnitText1.He']
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
     for collection in collections:
-        if collection == 'movies':
+        if collection != 'places':
             continue
         started = datetime.datetime.now()
         count = db[collection].count()
@@ -63,22 +28,15 @@ if __name__ == '__main__':
         print 'Collection {} has {} documents.'.format(collection, count)
         for doc in db[collection].find({}, snapshot=True):
             key = '{}.{}'.format(collection, doc['_id'])
-            ##related = api.get_bhp_related(doc)
-            related = []
-            for c in collections:
-                found_related = es_mlt_search(index_name, collection, doc['_id'], related_fields, c, 1)
-                if found_related:
-                    related.extend(found_related)
+            related = api.get_bhp_related(doc)
             if not related:
                 print 'No related items found for {}'.format(key)
+                doc['related'] = []
+                db[collection].save(doc)
                 continue
             else:
                 doc['related'] = related
                 db[collection].save(doc)
-                #save_redis_list(r, key, related)
-                #related = get_redis_list(r, key)
-                #related_list = [rr[2:-2] for rr in related]
-
 
         finished = datetime.datetime.now()
         per_doc_time = (finished - started).total_seconds()/count
