85f1e7288ca441320db3e7a0fb31be76fd840fbc
Benny Daon <bennydaon@gmail.com>
2016-07-03 12:03:03 +0300

    Separating the users' from the data endpoints
    the /login, /mjs, /user and / endpoints are not version dependent and are now
    part of `user_endpoints`

--- tests/test_mjs.py
+++ tests/test_mjs.py
@@ -10,13 +10,13 @@ def tester_headers(client, get_auth_header):
     return headers
 
 def test_mjs_in_user_data(client, request, tester_headers):
-    res = client.get('/v1/user',
+    res = client.get('/user',
                      headers=tester_headers)
     assert 'story_items' in res.json
     assert len(res.json['story_branches']) == 4
 
 def test_add_item_to_mjs(client, request, tester_headers):
-    res = client.post('/v1/mjs',
+    res = client.post('/mjs',
                 headers = tester_headers,
                 data = 'photoUnits.123')
     assert res.status_code == 200
@@ -26,19 +26,19 @@ def test_add_item_to_mjs(client, request, tester_headers):
     assert res.json['story_branches'] == 4*['']
 
 def test_mjs_rename_branch(client, tester_headers):
-    res = client.post('/v1/mjs/1/name', headers=tester_headers,
+    res = client.post('/mjs/1/name', headers=tester_headers,
                      data = 'mothers')
     assert res.status_code == 200
     assert res.json['story_branches'] == ['mothers', '', '', '']
 
 
 def test_illegal_add_to_branch(client, request, tester_headers):
-    res = client.post('/v1/mjs/1', headers=tester_headers,
+    res = client.post('/mjs/1', headers=tester_headers,
                      data = 'personalities.456')
     assert res.status_code == 400
 
 def test_add_to_branch(client, request, tester_headers):
-    res = client.post('/v1/mjs/1', headers=tester_headers,
+    res = client.post('/mjs/1', headers=tester_headers,
                      data = 'photoUnits.123')
     assert res.status_code == 200
     assert len(res.json['story_items']) == 1
@@ -47,7 +47,7 @@ def test_add_to_branch(client, request, tester_headers):
     assert item['in_branch'] == [True, False, False, False]
 
 def test_delete_from_branch(client, request, tester_headers):
-    res = client.delete('/v1/mjs/1/photoUnits.123', headers=tester_headers)
+    res = client.delete('/mjs/1/photoUnits.123', headers=tester_headers)
     assert res.status_code == 200
     assert len(res.json['story_items']) == 1
     item = res.json['story_items'][0]
@@ -55,6 +55,6 @@ def test_delete_from_branch(client, request, tester_headers):
     assert item['in_branch'] == [False, False, False, False]
 
 def test_delete_item_from_story(client, request, tester_headers):
-    res = client.delete('/v1/mjs/photoUnits.123', headers=tester_headers)
+    res = client.delete('/mjs/photoUnits.123', headers=tester_headers)
     assert res.status_code == 200
     assert len(res.json['story_items']) == 0
