cc375016586d26cb4ec5b92d3a7fd01db1f22a85
Benny Daon <bennydaon@gmail.com>
2016-08-15 14:57:56 +0300

    updating batch_related to use app context

--- batch_related.py
+++ batch_related.py
@@ -8,14 +8,15 @@ import argparse
 import elasticsearch
 from werkzeug.exceptions import NotFound, Forbidden
 
-from bhs_api import SEARCHABLE_COLLECTIONS, logger, client_data_db, es
-from bhs_api import data_db as default_db
+from bhs_common.utils import SEARCHABLE_COLLECTIONS
+from bhs_api import create_app
 from bhs_api.item import (SHOW_FILTER, Slug, get_item_slug,
                           get_item_by_id, get_item, get_collection_name,
                           get_item_query)
 from bhs_api.utils import uuids_to_str
 
 data_db = None
+es = None
 
 def reduce_related(related_list):
     reduced = {}
@@ -244,12 +245,15 @@ def parse_args():
 if __name__ == '__main__':
 
     args = parse_args()
+    app, conf = create_app(testing=True)
+    es = app.es
+    logger = app.logger
     collections = SEARCHABLE_COLLECTIONS
     logger.setLevel(logging.INFO)
     if args.db:
-        data_db = client_data_db[args.db]
+        data_db = app.client_data_db[args.db]
     else:
-        data_db = default_db
+        data_db = app.data_db
 
     query = SHOW_FILTER.copy()
     if args.slug:
@@ -257,84 +261,91 @@ if __name__ == '__main__':
             query.update({"Slug.En": args.slug})
         else:
             query.update({"Slug.He": args.slug})
-    logger.info('Pass 1 - Collecting bhp related')
-    direct_related_list = []
-    for collection in collections:
-        for doc in data_db[collection].find(query, modifiers={"$snapshot": "true"}):
-            related = get_bhp_related(doc, max_items=6, bhp_only=True)
-            if not related:
-                continue
+
+    with app.app_context():
+        logger.info('Pass 1 - Collecting bhp related')
+        direct_related_list = []
+        for collection in collections:
+            for doc in data_db[collection].find(query,
+                                                modifiers={"$snapshot": "true"}):
+                related = get_bhp_related(doc, max_items=6, bhp_only=True)
+                if not related:
+                    continue
+                else:
+                    item_slug = get_item_slug(doc)
+                    direct_related_list.append({item_slug: related})
+
+        logger.info('Pass 1 finished')
+
+        # Inverting the direction of related structures and adding the result
+        # to original
+        reverse_related_list = reverse_related(direct_related_list)
+        unified_related_list = unify_related_lists(direct_related_list,
+                                                   reverse_related_list)
+
+        # Save the related info for debug
+        if args.debug:
+            with open(args.filename, 'w') as fh:
+                import json
+                json.dump(unified_related_list, fh, indent=2)
+
+        logger.info('Pass 2 - Applying bhp related')
+
+        for item in unified_related_list:
+            key = item.keys()[0]
+            value = item.values()[0]
+            slug = Slug(key)
+            try:
+                doc = get_item(slug)
+            except (Forbidden, NotFound):
+                    continue
+            if doc:
+                data_db[collection].update_one(get_item_query(slug),
+                                            {'$set': {'bhp_related': value}})
             else:
-                item_slug = get_item_slug(doc)
-                direct_related_list.append({item_slug: related})
+                logger.info('Problem with {}'.format(key))
+                sys.exit(1)
 
-    logger.info('Pass 1 finished')
+        logger.info('Pass 2 finished')
 
-    # Inverting the direction of related structures and adding the result
-    # to original
-    reverse_related_list = reverse_related(direct_related_list)
-    unified_related_list = unify_related_lists(direct_related_list, reverse_related_list)
+        logger.info('Pass 3 - Completing related and enriching documents')
+        for collection in collections:
+            started = datetime.datetime.now()
+            count = data_db[collection].count(SHOW_FILTER)
+            logger.info('Starting to work on {}'.format(collection))
+            logger.info('Collection {} has {} valid documents.'
+                        .format(collection, count))
+            for doc in data_db[collection].find(query,
+                                                modifiers={"$snapshot": "true"}):
+                slug = get_item_slug(doc)
+                if not doc.has_key('bhp_related') or not doc['bhp_related']:
+                    # No bhp_related, get everything from es
+                    related = sort_related(get_es_text_related(doc,
+                                                items_per_collection=2))[:6]
+                elif len(doc['bhp_related']) < 6:
+                    # Not enough related items, get an addition from es
+                    es_related = get_es_text_related(doc, items_per_collection=1)
+                    bhp_related = doc['bhp_related']
+                    bhp_related.extend(es_related)
+                    related = sort_related(list(set(bhp_related)))[:6]
+                else:
+                    #  Sort and cut bhp_related
+                    related = sort_related(list(set(doc['bhp_related'])))[:6]
 
-    # Save the related info for debug
-    if args.debug:
-        with open(args.filename, 'w') as fh:
-            import json
-            json.dump(unified_related_list, fh, indent=2)
+                if not related:
+                    logger.debug('No related items found for {}'.format(
+                        slug.encode('utf8')))
+                doc['related'] = related
+                data_db[collection].update_one(get_item_query(slug),
+                                            {'$set': {'related': related}})
 
-    logger.info('Pass 2 - Applying bhp related')
+            finished = datetime.datetime.now()
+            try:
+                per_doc_time = (finished - started).total_seconds()/count
+                logger.info("""Finished working on {}.
+    Related took {:.2f} seconds per document.""".format(
+            collection, per_doc_time))
+            except ZeroDivisionError:
+                logger.warn("count is zero")
 
-    for item in unified_related_list:
-        key = item.keys()[0]
-        value = item.values()[0]
-        slug = Slug(key)
-        try:
-            doc = get_item(slug)
-        except (Forbidden, NotFound):
-                continue
-        if doc:
-            data_db[collection].update_one(get_item_query(slug),
-                                           {'$set': {'bhp_related': value}})
-        else:
-            logger.info('Problem with {}'.format(key))
-            sys.exit(1)
-
-    logger.info('Pass 2 finished')
-
-    logger.info('Pass 3 - Completing related and enriching documents')
-    for collection in collections:
-        started = datetime.datetime.now()
-        count = data_db[collection].count(SHOW_FILTER)
-        logger.info('Starting to work on {}'.format(collection))
-        logger.info('Collection {} has {} valid documents.'.format(collection, count))
-        for doc in data_db[collection].find(query, modifiers={"$snapshot": "true"}):
-            slug = get_item_slug(doc)
-            if not doc.has_key('bhp_related') or not doc['bhp_related']:
-                # No bhp_related, get everything from es
-                related = sort_related(get_es_text_related(doc, items_per_collection=2))[:6]
-            elif len(doc['bhp_related']) < 6:
-                # Not enough related items, get an addition from es
-                es_related = get_es_text_related(doc, items_per_collection=1)
-                bhp_related = doc['bhp_related']
-                bhp_related.extend(es_related)
-                related = sort_related(list(set(bhp_related)))[:6]
-            else:
-                #  Sort and cut bhp_related
-                related = sort_related(list(set(doc['bhp_related'])))[:6]
-
-            if not related:
-                logger.debug('No related items found for {}'.format(
-                    slug.encode('utf8')))
-            doc['related'] = related
-            data_db[collection].update_one(get_item_query(slug),
-                                           {'$set': {'related': related}})
-
-        finished = datetime.datetime.now()
-        try:
-            per_doc_time = (finished - started).total_seconds()/count
-            logger.info("""Finished working on {}.
-Related took {:.2f} seconds per document.""".format(
-        collection, per_doc_time))
-        except ZeroDivisionError:
-            logger.warn("count is zero")
-
-    logger.info('Pass 3 finished')
+        logger.info('Pass 3 finished')
