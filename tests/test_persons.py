583e7d12ad5988932791676fd3e68368e7a3a452
Benny Daon <bennydaon@gmail.com>
2016-09-25 12:03:52 +0300

    testing for einstein instead of douani

--- tests/test_persons.py
+++ tests/test_persons.py
@@ -7,6 +7,6 @@ from pytest_flask.plugin import client
 # The documentation for client is at http://werkzeug.pocoo.org/docs/0.9/test/
 
 def test_fsearch_api(client):
-    res = client.get('/v1/person?last_name=Douani')
+    res = client.get('/v1/person?last_name=Einstein')
     assert 'items' in res.json
     assert int(res.json['total']) > 5
