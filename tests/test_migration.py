f471693121e6aed0f5da223132369bf4529729fd
Benny Daon <bennydaon@gmail.com>
2016-11-10 10:43:46 +0200

    a bit of refactoring

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -3,6 +3,16 @@ import elasticsearch
 from migration.tasks import update_doc, update_tree
 from migration.files import upload_file
 
+first_tree = dict(num=1,
+                    file_id='1',
+                    persons=8,
+                    date='now')
+second_tree = dict(num=1,
+                    file_id='5',
+                    persons=16,
+                    date='right now')
+
+
 def test_update_doc(mocker, mock_db, app):
     mocker.patch('elasticsearch.Elasticsearch.index')
     collection = mock_db['personalities']
@@ -28,52 +38,41 @@ def test_update_photo(mocker):
     boto.storage_uri.assert_called_once_with('to_bucket/to_key', 'gs')
 
 def test_update_tree(mock_db, app):
+
     with app.app_context():
-        update_tree(dict(num=1,
-                        file_id='1',
-                        persons=8,
-                        date='now'),
+        update_tree(first_tree,
                     mock_db['trees'])
     tree = mock_db['trees'].find_one({'num': 1})
     assert tree['versions'][0]['file_id'] == '1'
     assert tree['versions'][0]['persons'] == 8
     # adding the same tree again shouldn't do anythong
     with app.app_context():
-        update_tree(dict(num=1,
-                        file_id='1',
-                        persons=8,
-                        date='now'),
+        update_tree(first_tree,
                     mock_db['trees'])
     tree = mock_db['trees'].find_one({'num': 1})
     assert len(tree['versions']) == 1
     # adding new version
     with app.app_context():
-        update_tree(dict(num=1,
-                        file_id='5',
-                        persons=16,
-                        date='right now'),
+        update_tree(second_tree,
                     mock_db['trees'])
     tree = mock_db['trees'].find_one({'num': 1})
     assert len(tree['versions']) == 2
 
 
 def test_update_person(mock_db, app):
-    collection = mock_db['persons']
+    persons = mock_db['persons']
     app.data_db = mock_db
     with app.app_context():
         # first, create a tree
-        update_tree(dict(num=1,
-                        file_id='1',
-                        persons=8,
-                        date='now'),
+        update_tree(first_tree,
                     mock_db['trees'])
         # and now the tree
-        update_doc(collection, {
+        update_doc(persons, {
             'id': 'I1',
             'tree_num': 1,
             'tree_file_id': '1'
             })
-    doc =  collection.find_one({'id':'I1'})
+        doc =  persons.find_one({'id':'I1'})
     assert doc['tree_version'] == 0
     # 
     with app.app_context():
@@ -84,9 +83,10 @@ def test_update_person(mock_db, app):
                     mock_db['trees'])
         # reset the cache
         app.redis.delete('tree_vers_1')
-        update_doc(collection, {
+        update_doc(persons, {
             'id': 'I1',
             'tree_num': 1,
             'tree_file_id': '2'
             })
-    assert collection.count({'id':'I1'}) == 2
+    assert persons.count({'id':'I1'}) == 2
+    # eensure the old I1 is archived
