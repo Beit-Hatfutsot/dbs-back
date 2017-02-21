d5a7ca489e352948a402a307edec45f45fa53448
Ori Hoch <ori@uumpa.com>
2017-02-21 10:37:22 +0200

    family tree unit tests

--- tests/test_trees.py
+++ tests/test_trees.py
@@ -18,6 +18,38 @@ def given_mock_migrate_environment(mocker):
     mocker.patch("pymssql.connect")
 
 
+def given_mock_gedcom_tree_with_single_person(deceased=True, birth_year=1679, death_year=1755, gender="F"):
+    gedcom = type('MockGedcom', (object,), {
+        "as_list": [
+            type('MockGedcomHead', (object,), {
+                "children": [
+                    type("MockGedcomChild", (object,), {"tag": "SOUR", "value": "FTW", "children": []}),
+                    type("MockGedcomChild", (object,), {"tag": "DATE", "value": "21 AUG 2008", "children": []}),
+                    type("MockGedcomChild", (object,), {"tag": "GEDC", "value": None, "children": [
+                        type("MockGedcomChild", (object,), {"tag": "VERS", "value": "5.5", "children": []}),
+                        type("MockGedcomChild", (object,), {"tag": "FORM", "value": "LINEAGE-LINKED", "children": []})
+                    ]})
+                ]
+            })
+        ],
+        "as_dict": {
+            "@I01@": type('MockGedcomPerson', (object,), {
+                "pointer": "@I01@", "is_individual": True,
+                "gender": gender,  # can be one of "F" / "M" / ""
+                "deceased": deceased,  # based on the gedcom DEAT attribute
+                "private": False,
+                "name": "Shneor the milk man",
+                "birth_year": birth_year,
+                "death_year": death_year,
+                "marriage_years": None,
+                "children": [],
+            }),
+        },
+        "families": lambda self, node, attr: [],
+        "get_parents": lambda self, e: []
+    })
+
+
 def test_migrate_trees(mocker):
     given_mock_migrate_environment(mocker)
     from scripts.migrate import migrate_trees, parse_doc, get_collection_id_field
@@ -41,3 +73,14 @@ def test_migrate_trees(mocker):
     assert saved_docs[0][3]["id"] == "I29"  # doc
     assert saved_docs[0][3]["deceased"] == False
     assert saved_docs[3][3]["deceased"] == True
+
+
+def test_gedcom_to_persons(mocker):
+    from migration.family_trees import Gedcom2Persons
+
+    tree_num = None
+    file_id = None
+    saved_data = []
+    on_save = lambda data, collection: saved_data.append(data)
+    Gedcom2Persons(gedcom(), tree_num, file_id, on_save)
+    print(saved_data)
