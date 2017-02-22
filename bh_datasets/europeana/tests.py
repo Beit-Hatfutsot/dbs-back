6307a70f1bf76a45a662c411ed8998347c5acb8b
Ori Hoch <ori@uumpa.com>
2017-02-22 15:46:53 +0200

    fix the mock requests

--- bh_datasets/europeana/tests.py
+++ bh_datasets/europeana/tests.py
@@ -1,24 +1,29 @@
 from bh_datasets.europeana.dataset import EuropeanaDataset
-from ..common.mocks import MockRequests, MockResponse
+from ..common.mocks import MockRequests, MockJsonResponse
 
 
-class MockRequestsCohenImages(MockRequests):
-
-    def get(self, url, params):
-        super(MockRequestsCohenImages, self).get(url, params)
-        return MockResponse()
+use_mock_requests = True  # you can change to False if you want to make the actual Europeana request
 
 
-def given_europeana_dataset_object(requests_module):
+def given_cohen_images_mock_requests():
+    requests = MockRequests()
+    requests.expect_get("http://www.europeana.eu/api/v2/search.json",
+                        {'query': 'cohen', 'rows': 5, 'start': 1, 'wskey': 'End3LH3bn',
+                         'qf': 'PROVIDER:"Judaica Europeana" TYPE:IMAGE'},
+                        MockJsonResponse({"success": True,
+                                          "itemsCount": 5,
+                                          "totalResults": 790,
+                                          "items": [{"title": "Kabalath Chabbath"},
+                                                    {}, {}, {}, {}]}))
+    return requests
+
+
+def given_europeana_dataset_object(requests_module=None):
     return EuropeanaDataset(wskey="End3LH3bn", requests_module=requests_module)
 
 
 def when_searching_for_cohen_images(europeana_dataset):
     return europeana_dataset.source_search(query="cohen", only_images=True, rows=5, start=1)
-
-
-def assert_http_request_to_euorpeana_for_cohen_images(requests_module):
-    assert requests_module.gets == []
 
 
 def assert_results_for_cohen_images(results):
@@ -29,15 +34,7 @@ def assert_results_for_cohen_images(results):
 
 
 def test_fetch():
-    requests = MockRequestsCohenImages()
+    requests = given_cohen_images_mock_requests() if use_mock_requests else None
     europeana = given_europeana_dataset_object(requests)
     results = when_searching_for_cohen_images(europeana)
-    assert_http_request_to_euorpeana_for_cohen_images(requests)
     assert_results_for_cohen_images(results)
-
-    # europeana_dataset = EuropeanaDataset(wskey="End3LH3bn", requests_module=requests)
-    # results = europeana_dataset.source_search(query="cohen", only_images=True, rows=5, start=1)
-    # assert results.itemsCount == 5
-    # assert results.totalResults == 790
-    # assert results.items[0].title == u'Kabalath Chabbath'
-    # assert len(results.items) == 5
