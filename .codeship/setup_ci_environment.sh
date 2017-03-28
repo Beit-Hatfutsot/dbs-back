8e7e67d95134d79c52d2287fa17842830dd08e51
Ori Hoch <ori@uumpa.com>
2017-03-28 22:56:53 +0300

    modify ci environment seutp

--- .codeship/setup_ci_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pip install -r requirements.txt
-pip install -r requirements.migrate.txt
-pip install pymssql==2.1.3
-jdk_switcher home oraclejdk8
-jdk_switcher use oraclejdk8
-scripts/setup_ci_environment.sh
