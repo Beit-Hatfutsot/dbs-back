2840202ab37ca8b578e94d0d7b185c5d41b9da34
Ori Hoch <ori@uumpa.com>
2017-04-04 12:34:16 +0300

    fix codeship setup

--- .codeship/setup.sh
+++ .codeship/setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+# following lines should be added directly on codeship setup code (not sure why..)
+# jdk_switcher home oraclejdk8
+# jdk_switcher use oraclejdk8
+
+pip install --upgrade pip
 pip install -r requirements.all.txt
-jdk_switcher home oraclejdk8
-jdk_switcher use oraclejdk8
 scripts/setup_ci_environment.sh
